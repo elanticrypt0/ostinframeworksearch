1bc04f88a25ceb0d8994519885bad8f19f0585bb
k23dev <hashersmasher@proton.me>
2024-10-26 19:40:05 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+import svelte from '@astrojs/svelte';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://astrofy-template.netlify.app',
+  integrations: [mdx(), sitemap(), tailwind(), svelte(), icon()]
+});
